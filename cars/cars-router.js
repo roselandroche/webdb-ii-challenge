5896f1a686b27afed311b8ef4b15f2707101cb0a
roselandroche <r.landroche@gmail.com>
2019-12-20 20:25:51 -0500

    Wrote out routes

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -3,6 +3,38 @@ const db = require("../utils/db")
 
 const router = express.Router()
 
-// routes
+router.get("/", (req, res, next) => {
+    try{
+        res.json(await db('cars').select())
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
+router.get("/:id", (req, res, next) => {
+    try{
+        res.json(await db('cars').where('id', req.params.id).first())
+    }
+    catch(err) {
+        next(err)
+    }
+})
+
+router.post("/", (req, res, next) => {
+    try{
+        const payload = {
+            VIN: req.body.VIN,
+            make: req.body.make,
+            model: req.body.model,
+            mileage: req.body.mileage
+        }
+        const [id] = await db('cars').insert(payload)
+        res.json(await db('cars').where('id', id).first())
+    }
+    catch(err) {
+        next(err)
+    }
+})
 
 module.exports = router
