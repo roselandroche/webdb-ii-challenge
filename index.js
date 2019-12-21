62e680c0c71d04ebedc23626e2292dd539843f6f
roselandroche <r.landroche@gmail.com>
2019-12-20 19:50:16 -0500

    Running into some error

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const helmet = require("helmet")
+const carsRouter = require("./cars/cars-router")
+
+const server = express()
+const HOST = process.env.HOST || "localhost"
+const PORT = process.env.PORT || 5000
+
+server.use(helmet())
+server.use(express.json())
+
+server.use("/api/cars", carsRouter)
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({ message: "Something went wrong." })
+})
+
+server.listen(PORT, HOST, () => {
+    console.log(`\nRunning on http://${HOST}:${PORT}\n`)
+})
---
+++ utils/db.js
@@ -0,0 +1,6 @@
+const knex = require("knex")
+const config = ("./knexfile.js")
+
+const db = knex(config.development)
+
+module.exports = db
